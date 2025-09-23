45f9ff1845781942e9537efd836e4f708d5e918c
bluesky <bluesky@example.com>
2025-09-23 16:51:34 +0900

    collabo react 2025/09/23

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -7,11 +7,11 @@ import FruitList from './../pages/FruitList';
 //import ElementList from '../pages/ElementList';
 import SignupPage from '../pages/SignupPage';
 import LoginPage from '../pages/LoginPage';
-import LogoutPage from '../pages/LogoutPage';
+import ProductList from '../pages/ProductList';
 
 // 이 파일은 라우팅 정보를 담고 있는 파일입니다.
 // 이러한 파일을 네트워크에서는 routing table이라고 합니다.
-function App({ user, handLoginSuccess, logout }) {
+function App({ user, handLoginSuccess }) {
     //user : 사용자 정보를 저장하고 있는 객체
     //handldLoginSuccess : 로그인 성공시 동작할 액션
     return (
@@ -19,10 +19,12 @@ function App({ user, handLoginSuccess, logout }) {
             {/* path 프롭스는 요청 정보 url, element 프롭스는 컴포넌트 이름 */}
             <Route path='/' element={<HomePage />} />
             <Route path='/member/signup' element={<SignupPage />} />
-            <Route path='/member/logout' element={<LogoutPage setUser={logout} />} />
+           
+           {/* 로그인 여부에 따라서 상품목록페이지가 다르게 보여야 하므로, user 프롭스를 넘겨줍니다.*/}
+            <Route path='/product/list' element={<productList/>} />
 
-            <Route path='/member/login' element={<LoginPage setUser={handLoginSuccess} />} />
-
+            <Route path='/fruit' element={<FruitOne />} />
+            
             <Route path='/fruit' element={<FruitOne />} />
             <Route path='/fruit/list' element={<FruitList />} />
             <Route path='/element' element={<ElementOne />} />
